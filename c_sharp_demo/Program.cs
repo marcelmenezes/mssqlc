ff962d9193d286f433ed8584d58dec5b95decc58
marcelmenezes <marcel.business@gmail.com>
2024-08-07 19:29:31 -0300

    exemplo c#

---
+++ c_sharp_demo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Starting SQL Server operations...");
+        
+        SqlServerOperations.RunDatabaseOperations();
+        
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+}
